9af6444814eba1af2ed0ca081df862c8a36ac260
henriesarts <henrymcmanus@lcad.edu>
2026-09-15 01:17:56 -0700

    added player movement and the cube (player)
    yayyyyy lets move tonight !

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public float moveSpeed = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            move += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move += Vector3.left;
+        }
+
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+}
