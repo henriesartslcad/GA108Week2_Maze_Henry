19102b95189f172421981b6d4d5a083f26443e95
henriesarts <henrymcmanus@lcad.edu>
2026-09-17 00:27:07 -0700

    added textures
    bigsmile sign and the cube

--- Assets/WinCondition.cs
+++ Assets/WinCondition.cs
@@ -1,4 +1,4 @@
-using System;
+
 using UnityEngine;
 
 public class WinCondition : MonoBehaviour
@@ -13,13 +13,16 @@ public class WinCondition : MonoBehaviour
     void Start()
     {
         Debug.Log("welcome to forest");
-        
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        //OKAY IM NOT GOING TO LIE IM NOT ENTIRELY SURE WHY THIS WORKS . but i figured i'd need to get the location
+        // of the objects and associate them with 'player' and 'goal' so i typed into google 'how to get location of 
+        // an object c# unity' and found a unity discussions forum post that had a script reference for find game object
+        // and the example was like . hand = GameObject.Find("Hand"); and then the .transform after it fetches the position
+        // it worked but i thought i was supposed to be doing something with vector3? maybe? but this works? haha
 
         goal = GameObject.Find("Goal").transform;
         player = GameObject.Find("Player").transform;
